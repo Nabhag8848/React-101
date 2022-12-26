92a01859e1fc2b1d952eeaaab823af1b2f383dbd
Nabhag Motivaras <nabhagmotivaras76@gmail.com>
2022-12-26 10:26:23 +0530

    feat: created basic blog app

--- src/App.js
+++ src/App.js
@@ -1,1 +1,17 @@
+import React from "react";
+import { PostCreate } from "./PostCreate";
+import { PostList } from "./PostList";
 
+export class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h1>Create Post</h1>
+        <PostCreate />
+        <hr />
+        <h1>Posts</h1>
+        <PostList />
+      </div>
+    );
+  }
+}
---
+++ src/CommentList.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export function CommentList(props) {
+  const [comments, setComments] = useState([]);
+
+  const fetchData = async () => {
+    const res = await axios.get(
+      `http://localhost:4001/posts/${props.postId}/comments`
+    );
+    setComments(res.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+  return <ul>{renderedComments}</ul>;
+}
